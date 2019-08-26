2fb99fd1e71b6715f9b46cf083afcbffeaaf9e0f
Chris Makler <chris.makler@gmail.com>
2019-08-25 18:13:56 -0700

    had loaded katex in the wrong order

--- bundle.py
+++ bundle.py
@@ -33,8 +33,8 @@ bundles = [
         "name": "kg-lib.js",
         "dest_directories": ["build/lib/"],
         "order": [
-            "node_modules/katex/dist/contrib/auto-render.min.js",
             "node_modules/katex/dist/katex.min.js",
+            "node_modules/katex/dist/contrib/auto-render.min.js",
             "node_modules/d3/dist/d3.min.js",
             "node_modules/mathjs/dist/math.min.js",
             "node_modules/js-yaml/dist/js-yaml.min.js"
