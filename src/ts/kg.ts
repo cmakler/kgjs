fa330ba66dd7d3b2e471e9e1138ba384fca21ff7
Chris Makler <chris.makler@gmail.com>
2022-06-23 10:45:21 -0400

    Update kg.ts

--- src/ts/kg.ts
+++ src/ts/kg.ts
@@ -60,7 +60,7 @@
 /// <reference path="view/mathboxObjects/mathboxCurve.ts" />
 /// <reference path="view/mathboxObjects/mathboxSurface.ts" />
 /// <reference path="view/mathboxObjects/mathboxDataSurface.ts" />
-/// <reference path="view/mathboxObjects/mathboxPlane.ts"
+/// <reference path="view/mathboxObjects/mathboxPlane.ts" />
 /// <reference path="view/mathboxObjects/mathboxShape.ts" />
 /// <reference path="view/mathboxObjects/mathboxLabel.ts" />
 /// <reference path="view/mathboxObjects/mathboxLine.ts" />
